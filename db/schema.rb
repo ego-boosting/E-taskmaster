9920cd099c8770161a4941df533bc110e5b9ffc0
EC2 Default User <kazukipump7@gmail.com>
2022-08-02 14:22:31 +0000

    [Update]model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_08_02_082641) do
+ActiveRecord::Schema.define(version: 2022_08_02_082540) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -22,15 +22,6 @@ ActiveRecord::Schema.define(version: 2022_08_02_082641) do
     t.datetime "updated_at", precision: 6, null: false
     t.index ["email"], name: "index_admins_on_email", unique: true
     t.index ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
-  end
-
-  create_table "contacts", force: :cascade do |t|
-    t.integer "use_id", null: false
-    t.string "title", null: false
-    t.text "body", null: false
-    t.text "reply", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "notes", force: :cascade do |t|
