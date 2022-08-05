b84cb604871e1286f17667bfc31a914dcea4db11
EC2 Default User <kazukipump7@gmail.com>
2022-08-05 16:29:24 +0000

    [Add]Kaminari

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+  # 検索機能
+  before_action :set_search
+
+  def set_search
+    @search = Note.ransack(params[:q]) #ransackメソッド推奨
+    @search_notes = @search.result.page(params[:page])
+  end
 end
+
