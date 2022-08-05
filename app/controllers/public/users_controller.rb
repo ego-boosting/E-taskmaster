3e88dfb0b8318ac4f772ce4e0a1608a14da56c2a
EC2 Default User <kazukipump7@gmail.com>
2022-08-05 14:58:07 +0000

    [Add]ransack

--- app/controllers/public/users_controller.rb
+++ app/controllers/public/users_controller.rb
@@ -1,9 +1,37 @@
 class Public::UsersController < ApplicationController
+  before_action :authenticate_user!
+  before_action :ensure_guest_user, only: [:edit]
 
   def show
     @user = current_user
   end
 
   def edit
+    @user = User.find(params[:id])
   end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to users_path
+      flash[:notice] = "更新が完了しました。"
+    else
+      render "edit"
+      flash[:alert] = "更新が更新ができませんでした。"
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :name)
+  end
+  
+  def ensure_guest_user
+    @user = User.find(params[:id])
+    if @user.name == "guestuser"
+      redirect_to users_path(current_user), notice: 'ゲストユーザーはプロフィール編集画面へ遷移できません。'
+    end
+  end
+  
 end
