674a162b34b5362276e3a89548236a0e1cb44d8e
EC2 Default User <kazukipump7@gmail.com>
2022-08-16 16:35:29 +0000

    [Add]アクセス制限

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,4 +1,5 @@
 class Admin::UsersController < ApplicationController
+  before_action :authenticate_admin!
 
   def index
     @users = User.where.not(name: "guestuser").page(params[:page]).per(10).order('updated_at DESC')
