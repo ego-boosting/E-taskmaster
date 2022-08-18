b9bad8595c34c9ee8db7b9ecd8cd45c22f9eda21
EC2 Default User <kazukipump7@gmail.com>
2022-08-18 09:41:54 +0000

    [Add]unsubscribe

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -8,11 +8,26 @@ class Admin::UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
   end
-  
-  def edit 
+
+  def edit
+    @user = User.find(params[:id])
   end
-  
+
   def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to admin_user_path
+    else
+      render "edit"
+    end
   end
+
+
+  private
+
+  def user_params
+      params.require(:user).permit(:email,:name,:is_deleted)
+  end
+
 
 end
