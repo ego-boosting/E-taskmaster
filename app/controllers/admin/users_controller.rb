81df2b5f16c97845232f5ddb3e7594c06c4bf640
EC2 Default User <kazukipump7@gmail.com>
2022-08-18 20:12:02 +0000

    [Add]HowtoImage

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,18 +1,22 @@
 class Admin::UsersController < ApplicationController
   before_action :authenticate_admin!
-  before_action :before_customer, only:[:show,:edit,:update]
 
   def index
     @users = User.where.not(name: "guestuser").page(params[:page]).per(10).order('updated_at DESC')
   end
 
   def show
+    @user = User.find(params[:id])
   end
 
   def edit
+    @user = User.find(params[:id])
+    @origin_user = User.find(params[:id])
   end
 
   def update
+    @user = User.find(params[:id])
+    @origin_user = User.find(params[:id])
     if @user.update(user_params)
       redirect_to admin_user_path
       flash[:notice] = "更新が完了しました。"
@@ -22,15 +26,9 @@ class Admin::UsersController < ApplicationController
     end
   end
 
-
   private
 
   def user_params
-      params.require(:user).permit(:email,:name,:is_deleted)
+    params.require(:user).permit(:email,:name,:is_deleted)
   end
-
-   def before_customer
-     @user = User.find(params[:id])
-  end
-
 end
