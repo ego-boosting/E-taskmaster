13b5d561048639e551615e76658488ec53d23be3
EC2 Default User <kazukipump7@gmail.com>
2022-08-28 11:22:15 +0000

    [Add]https化

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -117,17 +117,19 @@ Rails.application.configure do
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
-  Rails.application.configure do
-    config.action_mailer.raise_delivery_errors = true
-    config.action_mailer.delivery_method = :smtp
-    config.action_mailer.smtp_settings = {
-      address:              'smtp.gmail.com',
-      port:                  587,
-      domain:               'gmail.com',
-      user_name:            ENV["MAIL_ADDRESS"],
-      password:             ENV["SECRET_KEY"],
-      authentication:       'plain',
-      enable_starttls_auto:  true
-    }
-  end
+
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                  587,
+    domain:               'gmail.com',
+    user_name:            ENV["MAIL_ADDRESS"],
+    password:             ENV["SECRET_KEY"],
+    authentication:       'plain',
+    enable_starttls_auto:  true
+  }
+
+  config.action_controller.default_url_options = {host: "e-taskmaster.com"}
+  config.action_controller.asset_host = "https://e-taskmaster.com"
 end
