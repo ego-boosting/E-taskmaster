645f7bcf0d7169bed0b242abcd8a8b72eb753565
EC2 Default User <kazukipump7@gmail.com>
2022-09-16 16:16:07 +0000

    [Add]Association

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,16 +4,21 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-         has_many :notes, dependent: :destroy
-         has_many :tasks, dependent: :destroy
-         has_many :contacts, dependent: :destroy
-         has_many :favorites, dependent: :destroy
-         has_many :comments, dependent: :destroy
+      has_many :notes, dependent: :destroy
+      has_many :tasks, dependent: :destroy
+      has_many :contacts, dependent: :destroy
+      has_many :favorites, dependent: :destroy
+      has_many :comments, dependent: :destroy
+      has_many :relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
+      has_many :reverse_of_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
+      has_many :followings, through: :relationships, source: :followed
+      has_many :followers, through: :reverse_of_relationships, source: :follower
 
-         validates :name, length: { minimum: 2, maximum: 20 }
-         validates :email, presence: true
 
-         has_one_attached :profile_image
+      validates :name, length: { minimum: 2, maximum: 20 }
+      validates :email, presence: true
+
+      has_one_attached :profile_image
 
   def get_profile_image
     profile_image.attached? ? profile_image : 'fruits.png'
